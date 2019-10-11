9ebc6a740fd2185c71ad1185aa6270041d57bcc5
Alexander Yaskevich <yaskevich.san@mail.ru>
2019-10-11 19:24:45 +0300

    Fix problems with templates and iterators

--- Module1/Task1/main.cpp
+++ Module1/Task1/main.cpp
@@ -2,40 +2,44 @@
 #include <vector>
 #include <iterator>
 
-std::vector<size_t> computePrefixFunction( const std::string& pattern ) {
+template <typename PatternType, typename ElemType>
+size_t reusePrefixFunction( size_t compIndex,  const std::vector<size_t>& prefix,
+                            const PatternType& pattern,
+                            ElemType valueRequired ) {
+    while( compIndex > 0 && valueRequired != pattern[compIndex] ) {
+        compIndex = prefix[compIndex - 1];
+    }
+    return valueRequired == pattern[compIndex] ? compIndex + 1 : 0;
+}
+
+template <typename PatternType>
+std::vector<size_t> computePrefixFunction( const PatternType& pattern ) {
     std::vector<size_t> prefix(pattern.size());
     prefix[0] = 0;
     // Start with 1 because prefix[0] = 0 by definition
     for( size_t i = 1; i < pattern.size(); ++i ) {
-        size_t compIndex = prefix[i - 1];
-        while( compIndex > 0 && pattern[i] != pattern[compIndex] ) {
-            compIndex = prefix[compIndex - 1];
-        }
-        prefix[i] = (pattern[i] == pattern[compIndex]) ? (compIndex + 1) : 0;
+        prefix[i] = reusePrefixFunction(prefix[i - 1], prefix, pattern, pattern[i]);
     }
     return prefix;
 }
 
-void printMatches( const std::string& pattern, std::istream_iterator<char>& in,
-                   std::ostream_iterator<size_t>& out ) {
+template <typename BeginIterator, typename EndIterator, typename OutputIterator,
+        typename PatternType>
+void printMatches( const PatternType& pattern, BeginIterator begin, EndIterator end,
+                   OutputIterator out ) {
     std::vector<size_t> prefix = computePrefixFunction(pattern);
-    std::istream_iterator<char> endOfStream;
     size_t prevPos = 0; // The position we ended at before
     size_t indexCounter = 0; // Aka string index
-    while( in != endOfStream ) {
-        size_t compIndex = prevPos;
-        // Find the largest index where we can reuse prefix function
-        while( compIndex > 0 && pattern[compIndex] != *in ) {
-            compIndex = prefix[compIndex - 1];
-        }
-        compIndex = (pattern[compIndex] == *in) ? (compIndex + 1) : 0;
+    while( begin != end ) {
+        size_t compIndex = reusePrefixFunction(prevPos, prefix, pattern, *begin);
         // We found a match!
         if( compIndex == pattern.size() ) {
             // The index i - the length of pattern and "#"
             *out = indexCounter - pattern.size() + 1;
+            ++out;
         }
         prevPos = compIndex;
-        ++in;
+        ++begin;
         ++indexCounter;
     }
 }
@@ -49,8 +53,9 @@ int main() {
 
     std::ostream_iterator<size_t> out( std::cout, " " );
     std::istream_iterator<char> in( std::cin );
+    std::istream_iterator<char> eof;
 
-    printMatches(pattern, in, out);
+    printMatches(pattern, in, eof, out);
 
     return 0;
 }
