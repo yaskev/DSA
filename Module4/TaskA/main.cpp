7f8d99a0183f03c9b1bd14b0d955c179ad1a3207
Alexander Yaskevich <yaskevich.san@mail.ru>
2019-12-11 18:04:28 +0300

    class

--- Module4/TaskA/main.cpp
+++ Module4/TaskA/main.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include <vector>
+
+class BigInteger {
+public:
+	BigInteger(int);
+	friend operator+<>(const BigInteger& b1, const BigInteger& b2);
+
+};
 
 int main() {
 	std::cout << "Hello, World!" << std::endl;
